ebed2334d45f9405015f4e0abeacb743e48dc280
sakthisaravananT <140143912+sakthisaravananT@users.noreply.github.com>
2024-01-19 17:25:36 +0530

    Add files via upload

---
+++ choose.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Chat System</title>
+<body bgcolor="pink">
+    <font size="5pt" face="Calibri" color="black">
+</head><center>
+<br />
+<p><h1>Choose Below to Continue</h1><a><strong>
+<a href="@@cht">Group Chat</p></a>
+<br /><br>
+</br>
+<br>
+<br>
+</br><a href="logout.php">Log Out</a>
+<body>
+</body>
+<br /><br><br /><br><br /><br><br />
+</html>
